1f0c583c1aa854054c54ff48cc0892c2f57fbdb5
Eric <eric.lofgren03@gmail.com>
2025-10-07 10:01:58 +0200

    Fixed percentage displayed as decimal number...

--- floorball_stats/games/models.py
+++ floorball_stats/games/models.py
@@ -38,35 +38,35 @@ class Game(models.Model):
         if self.our_shots == 0: 
             return 0
 
-        return self.our_score / self.our_shots
+        return 100 * self.our_score / self.our_shots
     
     @property
     def save_percentage(self):
         if self.opponent_shots == 0: 
             return 0
 
-        return 1 - self.opponent_score / self.opponent_shots
+        return 100 * (1 - self.opponent_score / self.opponent_shots)
 
     @property
     def shot_save_percentage(self):
         if self.save_percentage == 0: 
             return 0
 
-        return self.shot_percentage / (1 - self.save_percentage)
+        return (self.shot_percentage / 100) / (1 - (self.save_percentage / 100))
 
     @property
     def powerplay_percentage(self):
         if self.powerplays == 0: 
             return 0
 
-        return self.powerplay_goals / self.powerplays
+        return 100 * self.powerplay_goals / self.powerplays
     
     @property
     def boxplay_percentage(self):
         if self.boxplays == 0: 
             return 0
 
-        return self.boxplay_goals_against / self.boxplays
+        return 100 * self.boxplay_goals_against / self.boxplays
     
     def __str__(self):
         return f"{self.date} vs {self.opponent}"
